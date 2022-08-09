615dbcdf1c92e6af9107e512ed38f424f03895fe
Eric da Costa <ericdacosta72@gmail.com>
2022-08-09 12:29:36 -0300

    primeiro commit

---
+++ Fundamentos de Programação de Computadores-exercícios/estrutura-sequencial/soma.js
@@ -0,0 +1,9 @@
+let calcSoma = (num1, num2, num3, num4) => num1 + num2 + num3 + num4;
+
+function main(){
+    let num1, num2, num3, num4;
+    let soma = calcSoma(num1, num2, num3, num4);
+    console.log(soma);
+}
+
+main()
