ad517e78233180f4c6ff26976b8c50fdb7bacb67
Eric da Costa <ericdacosta72@gmail.com>
2022-08-09 13:47:51 -0300

    Pega uma medida em pés e converte para outras medidas

---
+++ Fundamentos de Programação de Computadores-exercícios/estrutura-sequencial/conversao-em-pes.js
@@ -0,0 +1,10 @@
+function main(){
+    let medidaEmPes;
+    let polegadas = medidaEmPes / 12;
+    let jardas = 3 * medidaEmPes;
+    let milha = 1.760 * jardas;
+
+    console.log(polegadas, jardas, milha);
+}
+
+main();
