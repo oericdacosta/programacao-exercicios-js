e96a59329e3f75ca6b5269b6475af224ab028bab
Eric da Costa <ericdacosta72@gmail.com>
2022-08-10 16:09:39 -0300

    Uma espécie de calculadora que só faz duas operações

---
+++ Fundamentos de Programação de Computadores-exercícios/estrutura-condicional/mini-calculadora.js
@@ -0,0 +1,23 @@
+let somaNumeros = (x, y) => x + y;
+let calcRaizQuadrada = x => Math.sqrt(x); 
+
+let escolheOperacao = opcaoUsuario =>{
+    if(!opcaoUsuario || opcaoUsuario != 1 && opcaoUsuario != 2) return "Não é possível fazer essa operação";
+
+    let num1 = 4;
+    
+    if(opcaoUsuario === 1){
+        let num2 = 3;
+        return somaNumeros(num1, num2);
+    }
+
+    return calcRaizQuadrada(num1);
+}
+
+function main(){
+    let opcaoUsuario = 0;
+    let operacao = escolheOperacao(opcaoUsuario);
+    console.log(operacao);
+}
+
+main();
