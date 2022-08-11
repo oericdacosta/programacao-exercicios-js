911ab6dd52254bac259e855f66633f2a8a1dbb11
Eric da Costa <ericdacosta72@gmail.com>
2022-08-11 14:14:23 -0300

    calcula novo salário

---
+++ Fundamentos de Programação de Computadores-exercícios/estrutura-condicional/novo-salario.js
@@ -0,0 +1,21 @@
+let calcNovoSalario = (salario, bonificacao, auxilio) => salario + bonificacao + auxilio;
+
+let calcAuxilio = salario => (salario <= 600) ? 150 : 100;
+
+let calcBonificacao = salario =>{
+    if(salario <= 500) return salario * 0.05;
+    if(salario > 500 && salario <= 1200) return salario * 0.12;
+    return salario;
+}
+
+function main(){
+    let salario;
+    if(!salario) return;
+
+    let bonificacao = calcBonificacao(salario);
+    let auxilio = calcAuxilio(salario);
+    let novoSalario = calcNovoSalario(salario, bonificacao, auxilio);
+    console.log(novoSalario);
+}
+
+main();
