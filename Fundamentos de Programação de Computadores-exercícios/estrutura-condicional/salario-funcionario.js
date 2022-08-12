6d86333955b93c82f06a3c181888a8dd46d9e24f
Eric da Costa <ericdacosta72@gmail.com>
2022-08-11 23:13:39 -0300

    Calcula o salário de um funcionário e sua categoria

---
+++ Fundamentos de Programação de Computadores-exercícios/estrutura-condicional/salario-funcionario.js
@@ -0,0 +1,41 @@
+function verificarCategoria(salario){
+    if(salario <= 350) return "A";
+    if(salario > 350 && salario < 600) return "B";
+    return "C";
+}
+
+let calcSalarioLiquido = (salario, imposto, gratificacao) => salario - imposto + gratificacao;
+
+function calcGratificacao(salario, tempoServico){
+    if( salario > 0 && salario <= 500){
+        return (tempoServico > 0 && tempoServico <= 3) ? 20 : 50;
+    }
+    if(tempoServico > 0 && tempoServico <= 3) return 23;
+    if(tempoServico > 3 && tempoServico < 6) return 35;
+    return 33;
+}
+
+function calcImposto(salario){
+    if(salario < 200) return 0;
+    if(salario >= 200 && salario < 450) return salario * 0.03;
+    if(salario >= 450 && salario < 700) return salario * 0.08;
+    return salario * 0.12;
+}
+
+function main(){
+    let salarioBase;
+    salarioBase = salarioBase ?? 200;
+    if(Number.isNaN(salarioBase)) return;
+
+    let tempoServico;
+    tempoServico = tempoServico ?? 3;
+    if(Number.isNaN(tempoServico)) return;
+
+    let imposto = calcImposto(salarioBase);
+    let gratificacao = calcGratificacao(salarioBase, tempoServico);
+    let salarioLiquido = calcSalarioLiquido(salarioBase, imposto, gratificacao);
+    let categoria = verificarCategoria(salarioLiquido);
+    console.log(imposto, gratificacao, salarioLiquido, categoria);
+}
+
+main();
