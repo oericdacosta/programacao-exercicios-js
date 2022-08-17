a33eefb411287c06072e91c5854fa3bbc147adfe
Eric da Costa <ericdacosta72@gmail.com>
2022-08-17 09:24:17 -0300

    Remove valor de num

--- Fundamentos de Programação de Computadores-exercícios/estrutura-de-repeticao/valor-de-E.js
+++ Fundamentos de Programação de Computadores-exercícios/estrutura-de-repeticao/valor-de-E.js
@@ -2,7 +2,7 @@ function calculaFatorial(num){
     let fat = 1;
     if(num === 0) return fat;
     for(let i = 1; i <= num; i++){
-        fat = fat * i;
+        fat *= i;
     }
     return fat;
 }
@@ -11,14 +11,14 @@ function calculaE(num){
     let E = 1;
     for (let i = 0; i <= num; i++) {
         let fat = calculaFatorial(i);
-        E = E + 1/fat;
+        E += 1/fat;
     }
     console.log(E);
 
 }
 
 function main(){
-    let num = 0;
+    let num;
     calculaE(num);
 }
 
