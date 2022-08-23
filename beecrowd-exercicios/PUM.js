ff55373948f8335508c7e05b304ae677c4f6f4c2
Eric da Costa <ericdacosta72@gmail.com>
2022-08-23 09:48:50 -0300

    Apenas PUM

---
+++ beecrowd-exercicios/PUM.js
@@ -0,0 +1,24 @@
+function PUM(num){
+    let number =  0
+    let numbers = [];
+    let k = 0
+    for(let i = 1; i <= num; i++){
+        for(let j = 1; j <= 4; j++){
+            number++;
+            numbers.push(number);
+            if(number % 4 === 0){
+                numbers.pop();
+                numbers.push("PUM");
+            }
+            console.log(numbers[number - 1]);
+        }
+        console.log("");
+    }
+}
+
+function main(){
+    let num = 7;
+    PUM(num);
+}
+
+main();
